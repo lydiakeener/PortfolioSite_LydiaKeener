627bfd8c1719343eed99ba9882f3947c7741cf58
Lydia Keener <lydiakeener@gmail.com>
2020-09-15 17:12:25 -0400

    update script for hambuger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+
+
+function menuToggle(){
+  var x = document.getElementById('myNavtoggle');
+if (  x.className ==='navToggle') {
+  x.className += 'responsive';
+} else {
+  x.className = 'navToggle';
+}
+}
